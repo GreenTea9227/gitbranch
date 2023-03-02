3c31ab8c8dc7f20ef1743d18d72b6017d2db2980
jhs512 <jangka512@gmail.com>
2023-03-02 14:10:21 +0900

    숫자 유효성 체크

--- src/main/java/com/ll/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/wiseSaying/controller/WiseSayingController.java
@@ -1,6 +1,7 @@
 package com.ll.wiseSaying.controller;
 
 import com.ll.Container;
+import com.ll.Rq;
 import com.ll.wiseSaying.entity.WiseSaying;
 
 import java.util.ArrayList;
@@ -40,6 +41,16 @@ public class WiseSayingController {
         }
     }
 
-    public void remove() {
+    public void remove(Rq rq) {
+        int id = -1;
+
+        try {
+            id = Integer.parseInt(rq.getParam("id"));
+        } catch (NumberFormatException e) {
+            System.out.println("id(정수)를 입력해주세요.");
+            return;
+        }
+
+        System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
     }
 }
